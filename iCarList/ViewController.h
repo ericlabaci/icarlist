fae3ce41c091a1748d147b35d4b62b5e938b9d3a
Eric Labaci <eric.labaci@gmail.com>
2017-06-14 16:49:08 -0300

    First commit

---
+++ iCarList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCarList
+//
+//  Created by Eric Labaci on 6/14/17.
+//  Copyright © 2017 Eric Labaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iCarList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iCarList
+//
+//  Created by Eric Labaci on 6/14/17.
+//  Copyright © 2017 Eric Labaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
